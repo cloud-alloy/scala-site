420e6bfca199ab2addf0a8d9ca4d956e991be80b
RichardChester <richard.chester93@gmail.com>
2024-11-11 16:41:19 +0000

    bazel

--- src/main/scala/com/rockthejvm/Main.scala
+++ src/main/scala/com/rockthejvm/Main.scala
@@ -10,12 +10,22 @@ import zio.http.*
 import zio.logging.LogFormat
 import zio.logging.backend.SLF4J
 
+import scala.jdk.CollectionConverters._
+import java.io.File
+import java.net.{JarURLConnection, URL}
+import java.util.jar.JarFile
+
 object Main extends ZIOAppDefault {
 
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
     SLF4J.slf4j(LogFormat.colored)
 
   override def run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] = {
+
+    val resources = listResources("/") // Adjust if your resources are in a different path
+    println("Available resources:")
+    resources.foreach(println)
+
     val basicRoutes = Routes(
       Method.GET / "" -> handler(Response.redirect(Redirects.contacts)),
       Method.GET / "static" / "css" / "main.css" -> Handler.fromResource("css/main.css").orDie
@@ -50,4 +60,35 @@ object Main extends ZIOAppDefault {
       )
       .exitCode
   }
+
+
+  def listResources(path: String): Seq[String] = {
+    val resources = getClass.getClassLoader.getResources(path).asScala
+    resources.flatMap { url =>
+      url.getProtocol match {
+        case "file" => listFilesInDirectory(new File(url.toURI))
+        case "jar"  => listFilesInJar(url, path)
+        case _      => Seq.empty
+      }
+    }.toSeq
+  }
+
+  private def listFilesInDirectory(dir: File): Seq[String] = {
+    if (dir.exists && dir.isDirectory) {
+      dir.listFiles.flatMap {
+        case file if file.isDirectory => listFilesInDirectory(file)
+        case file                     => Seq(file.getPath)
+      }
+    } else Seq.empty
+  }
+
+  private def listFilesInJar(url: URL, path: String): Seq[String] = {
+    val jarConn = url.openConnection().asInstanceOf[JarURLConnection]
+    val jarFile = jarConn.getJarFile
+    jarFile.entries().asScala
+      .map(_.getName)
+      .filter(_.startsWith(path))
+      .toSeq
+  }
+
 }
